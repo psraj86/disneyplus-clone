d8895d9de76674594203a2a56d3f042cdb791328
Praveen Singh <psraj86@gmail.com>
2022-02-09 08:27:02 +0530

    ADDED: ImgSlider,Trendings,NewDisney, Originals, Recommends, Viewers etc

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,41 @@
-import styled from 'styled-components';
-import ImgSlider from './ImgSlider';
+import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
+import NewDisney from "./NewDisney";
+import Originals from "./Originals";
+import Recommends from "./Recommends";
+import Trendings from "./Trendings";
+import Viewers from "./Viewers";
 
 const Home = (props) => {
-    return (
-        <Container>
-            <ImgSlider />
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <ImgSlider />
+      <Viewers />
+      <Recommends />
+      <NewDisney />
+      <Originals />
+      <Trendings />
+    </Container>
+  );
+};
 
 const Container = styled.main`
-    position: relative;
-    min-height: calc(100vh - 250px);
-    overflow-x:hidden;
-    display:block;
-    top:72px;
-    padding: 0 calc(3.5vw + 5px);
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
 
-    &:after{
-        background: url('/images/home-background.png') center center / cover no-repeat fixed;
-        content: '';
-        position:absolute;
-        inset: 0px;
-        opacity:1;
-        z-index:-1
-    }
+  &:after {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
 `;
 
-export default Home
+export default Home;
